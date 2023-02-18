dcb36a871fcfbf3517f1212df5dfeea618ce885a
ciccio42 <frosa@unisa.it>
2023-02-18 11:29:45 +0100

    mosaic setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Visual One-shot Imitation',
     version='0.0.1',
-    packages=['mosaic'],
+    packages=find_packages(),
 )
